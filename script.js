0a7a186076b4ad662ea065267743fd94f415b6ef
Tony Saint-Amour <tonysaint-amour@Spooky-Mac.local>
2022-06-30 17:46:40 -0400

    14 of 50

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const toggle = document.getElementById('toggle')
+const nav = document.getElementById('nav')
+
+toggle.addEventListener('click', ()=>{
+    nav.classList.toggle('active')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
